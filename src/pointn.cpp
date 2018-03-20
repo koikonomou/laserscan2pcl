d6a6928d2235f95f78bf8f5d8d53b1bc2ec29218
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-03-20 15:21:24 +0200

    pointcloud_layers

--- src/pointn.cpp
+++ src/pointn.cpp
@@ -1,58 +1,59 @@
 #include <ros/ros.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
 #include <sensor_msgs/PointCloud.h>
+#include <pcl/impl/point_types.hpp>
+#include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/ChannelFloat32.h>
 #include <pcl/common/projection_matrix.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl/impl/point_types.hpp>
+#include <sensor_msgs/point_cloud_conversion.h>
 
+int counter = 0;
 ros::Publisher point_pub;
+int no_laserscans = 0;
+sensor_msgs::PointCloud2 accumulator;
 
+void cloud_callback (sensor_msgs::PointCloud2Ptr msg){
 
-void pointcloud_cb (const sensor_msgs::PointCloudConstPtr &msg ){
+	sensor_msgs::PointCloud pc1;
 
-sensor_msgs::PointCloud pointmsg;
-//pcl::PointCloud<pcl::PointXYZ>::iterator it;
-//pcl::PointXYZRGBA point_n;
-//pcl::PointCloud<pcl::PointXYZ> cloud;
+	sensor_msgs::convertPointCloud2ToPointCloud (*msg, pc1);
 
-pointmsg.header.seq = msg->header.seq;
-pointmsg.header.stamp = ros::Time::now();
-pointmsg.header.frame_id = msg->header.frame_id;
-pointmsg.points = msg->points;
-pointmsg.channels = msg->channels;
-pointmsg.points[0].x = msg->points[0].x;
-pointmsg.points[1].y = msg->points[1].y;
-pointmsg.points[2].z = msg->points[2].z;
-
-
-/*
-if (msg->points[2].z == 0){
-		pointmsg.points[2].z = 4.0;
+	for(size_t i=0; i<pc1.points.size(); i++) {
+		pc1.points[i].z = ((float)counter)/10.f;
 	}
-	//pointmsg.point[2].z = points[1].z;
 
+	sensor_msgs::convertPointCloudToPointCloud2(pc1, *msg); 
 
-/*
+	if( counter == 0 ) {
+		accumulator = sensor_msgs::PointCloud2(*msg);
+	}
+	else {
+		pcl::concatenatePointCloud( accumulator, *msg, accumulator );
 
-float it;
-for (it = cloud.points.begin(); it < cloud.points.end(); it++){
-	point_n.x = it->x;
-	point_n.y = it->y;
-	point_n.z = it->z;
+		if( counter == no_laserscans-1 ) {
+			counter = -1;
+			point_pub.publish( accumulator );
+		}
 
- }*/
-
-
-point_pub.publish (msg);
+	}
+	counter++;
 }
 
 int main (int argc, char** argv){
 	ros::init (argc, argv, "new_point_pointmsg");
 	ros::NodeHandle n_;
+	no_laserscans = 0;
+	std::string topic;
+	std::string out_topic;
+	n_.param("new_point_pointmsg/number_of_laserscans", no_laserscans, 10);
+	n_.param("new_point_pointmsg/cloud_topic", topic, std::string("/pointcloud2"));
+	n_.param("new_point_pointmsg/output_cloud_topic", out_topic, std::string("/new_point_cloud"));
 
-	ros::Subscriber sub = n_.subscribe ("/pointcloud", 1, pointcloud_cb);
+	ros::Subscriber sub = n_.subscribe (topic, 1, cloud_callback);
 
-	point_pub = n_.advertise<sensor_msgs::PointCloud> ("/new_point_cloud", 1);
+	point_pub = n_.advertise<sensor_msgs::PointCloud2> (out_topic, 1);
 
 	ros::spin ();
 }
