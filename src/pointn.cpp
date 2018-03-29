f07277bf7dba1f3e758e1083153a33a57aab7818
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-03-29 17:04:34 +0300

    changed

--- src/pointn.cpp
+++ src/pointn.cpp
@@ -9,45 +9,72 @@
 #include <pcl/common/projection_matrix.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 
-int counter = 0;
+
+ros::Time prev_time, my_time ;
+double off_set;
+int size;
+
+
 ros::Publisher point_pub;
-int no_laserscans = 0;
 sensor_msgs::PointCloud2 accumulator;
+std::vector<sensor_msgs::PointCloud> v;
+
+
+sensor_msgs::PointCloud2 bufferToAccumulator(const std::vector<sensor_msgs::PointCloud> buffer, double offset){
+	sensor_msgs::PointCloud2 accumulator;
+
+	sensor_msgs::PointCloud pcl_;
+	for(unsigned j=0; j < buffer.size(); j++){
+
+		sensor_msgs::PointCloud pc1 = sensor_msgs::PointCloud(buffer[j]);
+		my_time = pc1.header.stamp;
+		if (prev_time == my_time){
+			prev_time = my_time;
+		}
+		boost::chrono::system_clock::time_point before = boost::chrono::system_clock::now(); 
+
+		if (j > 0){
+			for(size_t i=0; i < pc1.points.size(); i++) {
+				pc1.points[i].z = pcl_.points[0].z + ((ros::Duration)(my_time - prev_time)).toSec() * offset;
+			}
+		}
+		pcl_ = sensor_msgs::PointCloud(pc1);
+		// boost::chrono::system_clock::time_point now = boost::chrono::system_clock::now();
+
+		sensor_msgs::PointCloud2 pc2;
+		sensor_msgs::convertPointCloudToPointCloud2(pc1, pc2);
+		pcl::concatenatePointCloud( accumulator, pc2, accumulator);
+		// boost::chrono::nanoseconds t = boost::chrono::duration_cast<boost::chrono::nanoseconds>(now-before);
+		prev_time = my_time;
+	}
+
+	return accumulator;
+
+}
 
 void cloud_callback (sensor_msgs::PointCloud2Ptr msg){
 
 	sensor_msgs::PointCloud pc1;
-
 	sensor_msgs::convertPointCloud2ToPointCloud (*msg, pc1);
 
-	for(size_t i=0; i<pc1.points.size(); i++) {
-		pc1.points[i].z = ((float)counter)/10.f;
+
+	// Replace vector with list for optimization
+	v.push_back(pc1);
+	if (v.size() > size){
+		v.erase(v.begin());
+		point_pub.publish(bufferToAccumulator(v, off_set));
 	}
 
-	sensor_msgs::convertPointCloudToPointCloud2(pc1, *msg); 
-
-	if( counter == 0 ) {
-		accumulator = sensor_msgs::PointCloud2(*msg);
-	}
-	else {
-		pcl::concatenatePointCloud( accumulator, *msg, accumulator );
-
-		if( counter == no_laserscans-1 ) {
-			counter = -1;
-			point_pub.publish( accumulator );
-		}
-
-	}
-	counter++;
 }
 
 int main (int argc, char** argv){
 	ros::init (argc, argv, "new_point_pointmsg");
 	ros::NodeHandle n_;
-	no_laserscans = 0;
+
 	std::string topic;
 	std::string out_topic;
-	n_.param("new_point_pointmsg/number_of_laserscans", no_laserscans, 10);
+	n_.param("new_point_pointmsg/offset", off_set, 1.0);
+	n_.param("new_point_pointmsg/offset", size, 40);
 	n_.param("new_point_pointmsg/cloud_topic", topic, std::string("/pointcloud2"));
 	n_.param("new_point_pointmsg/output_cloud_topic", out_topic, std::string("/new_point_cloud"));
 
