12999db504ac9fd5773c322b39bd4ba532f6feff
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-04-19 16:59:52 +0300

    changed publisher

--- src/laserscan_topcl.cpp
+++ src/laserscan_topcl.cpp
@@ -4,6 +4,7 @@
 #include <tf/message_filter.h>
 #include <pcl_ros/point_cloud.h>
 #include <tf/transform_listener.h>
+#include <my_new_msgs/clustering.h>
 #include <sensor_msgs/PointCloud.h>
 #include <pcl/impl/point_types.hpp>
 #include <sensor_msgs/PointCloud2.h>
@@ -15,25 +16,19 @@
 
 
 class LaserScanToPointCloud2{
-public:
 
+public:
+  int size;
+  double factor;
   ros::Publisher pub;
   ros::NodeHandle node;
-  ros::Time prev_time, my_time ;
-
-  sensor_msgs::PointCloud2 accumulator;
   std::vector<sensor_msgs::PointCloud> v;
+
   laser_geometry::LaserProjection projector;
   message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub;
 
   tf::TransformListener tfListener;
   tf::MessageFilter<sensor_msgs::LaserScan> laser_transform;
-  
-  int size = 40;
-  double factor = 1.0;
-
-
-
 
   LaserScanToPointCloud2(ros::NodeHandle n) : 
     node(n),
@@ -42,69 +37,68 @@ public:
   {
     laser_transform.registerCallback(boost::bind(&LaserScanToPointCloud2::scanCallback, this, _1));
     laser_transform.setTolerance(ros::Duration(0.01));
-    pub = node.advertise<sensor_msgs::PointCloud2> ("/my_pointcloud", 1);
+    pub = node.advertise<my_new_msgs::clustering> ("/my_pointcloud", 1);
 
   }
 
-  sensor_msgs::PointCloud2 bufferToAccumulator(const std::vector<sensor_msgs::PointCloud> buffer, double factor){
+    my_new_msgs::clustering bufferToAccumulator(std::vector<sensor_msgs::PointCloud> v_){
     sensor_msgs::PointCloud2 accumulator;
 
-    sensor_msgs::PointCloud pcl_;
-    for(unsigned j=0; j < buffer.size(); j++){
+    for(unsigned j=0; j < v_.size(); j++){
 
-      sensor_msgs::PointCloud pc1 = sensor_msgs::PointCloud(buffer[j]);
-      my_time = pc1.header.stamp;
-      if (prev_time == my_time){
-        prev_time = my_time;
-      }
-      boost::chrono::system_clock::time_point before = boost::chrono::system_clock::now(); 
+      //sensor_msgs::PointCloud pc1 = sensor_msgs::PointCloud(v[j]);
 
+      // boost::chrono::system_clock::time_point before = boost::chrono::system_clock::now(); 
       if (j > 0){
-        for(size_t i=0; i < pc1.points.size(); i++) {
-          pc1.points[i].z = pcl_.points[0].z + (ros::Duration(my_time - prev_time)).toSec() * factor;
+        for(size_t i=0; i < v_[j].points.size(); i++) {
+          v_[j].points[i].z = v_[j-1].points[0].z + ros::Duration(v_[j].header.stamp - v_[j-1].header.stamp).toSec() * factor;
         }
       }
-      pcl_ = sensor_msgs::PointCloud(pc1);
       // boost::chrono::system_clock::time_point now = boost::chrono::system_clock::now();
 
       sensor_msgs::PointCloud2 pc2;
-      sensor_msgs::convertPointCloudToPointCloud2(pc1, pc2);
+      sensor_msgs::convertPointCloudToPointCloud2(v_[j], pc2);
       pcl::concatenatePointCloud( accumulator, pc2, accumulator);
       // boost::chrono::nanoseconds t = boost::chrono::duration_cast<boost::chrono::nanoseconds>(now-before);
-      prev_time = my_time;
       }
 
-    return accumulator;
+      my_new_msgs::clustering c;
+      c.header.stamp = ros::Time::now();
+      c.clusters.push_back(accumulator);
+      c.factor = factor;
+      c.first_stamp = v_[0].header.stamp;
+
+    return c;
   }
 
 
- void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
-  {
+ void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in){
     sensor_msgs::PointCloud2 cloud;
     projector.transformLaserScanToPointCloud("base_link", *scan_in, cloud, tfListener);
 
     sensor_msgs::PointCloud pc1;
     sensor_msgs::convertPointCloud2ToPointCloud (cloud, pc1);
-
-
+    // TODO Investigate future problems (?)
+    pc1.header.stamp = ros::Time::now();
     v.push_back(pc1);
     if (v.size() > size){
         v.erase(v.begin());
-        pub.publish(bufferToAccumulator(v, factor));
+        pub.publish(bufferToAccumulator(v));
     }
 
-  }
-
+}
 
 };
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
   
   ros::init(argc, argv, "my_scan_to_cloud");
   ros::NodeHandle n;
-
   LaserScanToPointCloud2 lstopc(n);
+
+  n.param("my_scan_to_cloud/factor", lstopc.factor, 1.0);
+  n.param("my_scan_to_cloud/size", lstopc.size, 40);
+  // lstopc.set_values(40, 5.0);
   ros::spin();
   
   return 0;
